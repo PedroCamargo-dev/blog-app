ec0c36fc5add5dff657e52aaf425851fdbde0497
Pedro Camargo <pedrocamargo@MacBook-Pro-de-Pedro.local>
2023-05-15 23:15:51 -0300

    feat: create presentation new post

---
+++ src/modules/posts/presentation/components/newPost/index.tsx
@@ -0,0 +1,9 @@
+import EditorMD from '@/modules/posts/presentation/components/newPost/components/EditorMD'
+
+export default function NewPost() {
+  return (
+    <div className="-mt-10 flex justify-center">
+      <EditorMD />
+    </div>
+  )
+}
