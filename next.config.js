90f2e4d6ec5686d88a1f81b7f03b30256bc49d02
Pedro Camargo <pedrocamargo@MacBook-Pro-de-Pedro.local>
2023-05-15 23:28:16 -0300

    chore: add env next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    HOST_BACKEND: process.env.HOST_BACKEND,
+  },
   output: 'standalone',
   typescript: {
     ignoreBuildErrors: true,
