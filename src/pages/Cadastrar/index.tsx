6633250dcbd03b37fd6ff167a5843c7c8c8db9a0
Pedro Camargo <pedrocamargo@MacBook-Pro-de-Pedro.local>
2023-05-15 23:12:17 -0300

    fix: alter name page Cadastrar

--- src/pages/Cadastrar/index.tsx
+++ src/pages/Cadastrar/index.tsx
@@ -2,7 +2,7 @@ import Navbar from '@/common/components/Navbar'
 import Login from '@/modules/auth/presentation/components/login'
 import Register from '@/modules/auth/presentation/components/register'
 
-export default function LoginPage() {
+export default function RegisterPage() {
   return (
     <>
       <Navbar />
