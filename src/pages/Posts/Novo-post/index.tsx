c3825141d542e64350f6769873ed3342a37eb4ad
Pedro Camargo <pedrocamargo@MacBook-Pro-de-Pedro.local>
2023-05-15 23:17:22 -0300

    feat: create page new post

---
+++ src/pages/Posts/Novo-post/index.tsx
@@ -0,0 +1,11 @@
+import Navbar from '@/common/components/Navbar'
+import NewPost from '@/modules/posts/presentation/components/newPost'
+
+export default function NewPostPage() {
+  return (
+    <>
+      <Navbar />
+      <NewPost />
+    </>
+  )
+}
