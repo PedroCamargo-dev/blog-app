7abd885068ef081fb7cdd2a08496824d92b14a5d
Pedro Camargo <pedrocamargo@MacBook-Pro-de-Pedro.local>
2023-05-15 23:10:01 -0300

    fix: add onClick component button

--- src/common/components/Button/index.tsx
+++ src/common/components/Button/index.tsx
@@ -1,9 +1,10 @@
 import { ButtonProps } from '@/common/interfaces/components/IButton'
 
-export default function Button({ label, extraClass }: ButtonProps) {
+export default function Button({ label, extraClass, onClick }: ButtonProps) {
   return (
     <button
       className={`${extraClass} w-full rounded-sm text-center text-sm duration-300`}
+      onClick={onClick}
     >
       {label}
     </button>
