829b5d16f10ccf5132d82cd47f236b515d6121c4
Pedro Camargo <pedrocamargo@MacBook-Pro-de-Pedro.local>
2023-06-13 22:11:38 -0300

    fix: add type in component button

--- src/common/components/Button/index.tsx
+++ src/common/components/Button/index.tsx
@@ -1,8 +1,14 @@
 import { ButtonProps } from '@/common/interfaces/components/IButton'
 
-export default function Button({ label, extraClass, onClick }: ButtonProps) {
+export default function Button({
+  label,
+  extraClass,
+  onClick,
+  type,
+}: ButtonProps) {
   return (
     <button
+      type={type}
       className={`${extraClass} w-full rounded-sm text-center text-sm duration-300`}
       onClick={onClick}
     >
