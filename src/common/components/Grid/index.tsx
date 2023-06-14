b077ac2070c338a09b53246eed100392a95417b3
Pedro Camargo <pedrocamargo@MacBook-Pro-de-Pedro.local>
2023-06-13 22:14:08 -0300

    refactor: tailwind dynamic cols in component grid

--- src/common/components/Grid/index.tsx
+++ src/common/components/Grid/index.tsx
@@ -4,13 +4,57 @@ export default function Grid({
   cols,
   smcols,
   mdcols,
+  lgcols,
+  xlcols,
   gap,
-  extraClass,
   children,
+  extraClass,
 }: GridProps) {
+  const minCols: Record<number, string> = {
+    1: 'grid-cols-1',
+    2: 'grid-cols-2',
+  }
+
+  const smCols: Record<number, string> = {
+    1: 'sm:grid-cols-1',
+    2: 'sm:grid-cols-2',
+    3: 'sm:grid-cols-3',
+    4: 'sm:grid-cols-4',
+  }
+
+  const mdCols: Record<number, string> = {
+    1: 'md:grid-cols-1',
+    2: 'md:grid-cols-2',
+    3: 'md:grid-cols-3',
+    4: 'md:grid-cols-4',
+  }
+
+  const lgCols: Record<number, string> = {
+    1: 'lg:grid-cols-1',
+    2: 'lg:grid-cols-2',
+    3: 'lg:grid-cols-3',
+    4: 'lg:grid-cols-4',
+  }
+
+  const xlCols: Record<number, string> = {
+    1: 'xl:grid-cols-1',
+    2: 'xl:grid-cols-2',
+    3: 'xl:grid-cols-3',
+    4: 'xl:grid-cols-4',
+  }
+
   return (
     <div
-      className={`grid grid-cols-${cols} gap-${gap} sm:grid-cols-${smcols} md:grid-cols-${mdcols} ${extraClass}`}
+      className={`
+        grid
+        gap-${gap} 
+        ${extraClass}  
+        ${minCols[cols]}
+        ${smCols[smcols]}
+        ${mdCols[mdcols]}
+        ${lgCols[lgcols]}
+        ${xlCols[xlcols]}
+      `}
     >
       {children}
     </div>
